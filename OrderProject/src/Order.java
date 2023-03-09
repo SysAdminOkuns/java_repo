b7e52cc08022e256f6178da5a6c0d8de46a409e9
SysAdminOkuns <sysad.okundaye@gmail.com>
2023-03-09 11:38:59 -0500

    Added a order class and orderTest classes to the project.

---
+++ OrderProject/src/Order.java
@@ -0,0 +1,50 @@
+
+public class Order {
+	MyDate orderDate;
+	double orderAmount = 0.00;
+	String customer;
+	String product;
+	int quantity;
+	static double taxRate;
+	
+	static {
+		taxRate = 0.05;
+	}
+	
+	public Order(MyDate d, double amt, String c){
+		this.orderDate=d;
+		this.orderAmount=amt;
+		this.customer=c;
+		this.product="Anvil";
+		this.quantity=1;
+	}
+	
+	public Order(MyDate d, double amt, String c, String p, int q){
+		this.orderDate=d;
+		this.orderAmount=amt;
+		this.customer=c;
+		this.product=p;
+		this.quantity=q;
+	}
+	
+	public String toString(){
+		return quantity + " ea. " + product + " for " + customer; 
+	}
+	
+	public static void setTaxRate(double newRate) {
+		taxRate = newRate;
+	}
+	
+	public static void computeTaxOn(double anAmount) {
+		System.out.println("The tax for " + anAmount + " is: " + anAmount * Order.taxRate);
+	}
+	
+	public double computeTax() {
+		System.out.println("The tax for this order is: " + orderAmount * Order.taxRate);
+		return orderAmount * Order.taxRate;
+	}
+	
+	
+	
+	
+}
