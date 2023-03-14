71d09651f1edff2f9f81251e958a05942564e970
SysAdminOkuns <sysad.okundaye@gmail.com>
2023-03-14 19:17:47 -0400

    Updated the TestOrder class to include LocalDateTime for checking late orders

--- AcmeOrderSystem/src/com/acme/testing/TestOrders.java
+++ AcmeOrderSystem/src/com/acme/testing/TestOrders.java
@@ -1,5 +1,7 @@
 package com.acme.testing;
 
+import java.time.LocalDate;
+
 import com.acme.domain.Good.*;
 import com.acme.domain.Service;
 import com.acme.domain.Order;
@@ -17,6 +19,26 @@ public class TestOrders {
 		Order anvil = new Order(date1, 2000.00, "Wile E Coyote", s1, 10);
 		MyDate date2 = new MyDate(4, 10, 2008);
 		Solid s2 = new Solid("Acme Balloon", 1401, 15, UnitOfMeasureType.CUBIC_FEET, false, 10, 5, 5);
+
+		// Change this date to one that is within
+		// the last 15 days of today.
+		MyDate hammerDate = new MyDate(5, 17, 2016);
+		Solid hammerType = new Solid("Acme Hammer", 281, 0.3, UnitOfMeasureType.CUBIC_METER, false, 100, 0.25, 0.3);
+		Order hammer = new Order(hammerDate, 10.00, "Wile E Coyote", hammerType, 10);
+		Order.setRushable((orderDate, orderAmount) -> {
+			// Create a LocalDate object for now.
+			LocalDate now = LocalDate.now();
+			// Create a LocalDate object for the order date.
+			LocalDate orderDatePlus30 = LocalDate.of(orderDate.getYear(), orderDate.getMonth(), orderDate.getDay());
+			// Add one month to the order date.
+			orderDatePlus30 = orderDatePlus30.plusMonths(1);
+			// If the current date is after the order date + one month, // it's rushable.
+			return now.isAfter(orderDatePlus30);
+		});
+
+		System.out.println("Anvil isPriorityOrder: " + anvil.isPriorityOrder()); // True
+		System.out.println("Hammer isPriorityOrder: " + hammer.isPriorityOrder()); // False
+
 		Order balloons = new Order(date2, 1000.00, "Bugs Bunny", s2, 125);
 		Order.setRushable((orderDate, orderAmount) -> orderAmount > 1500);
 		System.out.println("Anvil isPriorityOrder: " + anvil.isPriorityOrder());
